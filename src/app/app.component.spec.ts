67b63d3e67882966d4dfa4e2024547ce919aa7d5
Gonzalo <gonzalo@pop-os.localdomain>
2022-07-01 09:48:54 +0200

    project name circular-portfolio --> ran

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'circular-portfolio'`, () => {
+  it(`should have as title 'ran'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('circular-portfolio');
+    expect(app.title).toEqual('ran');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('circular-portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ran app is running!');
   });
 });
